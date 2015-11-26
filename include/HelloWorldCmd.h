f18d5e37d4299c6ff21b63ea8ba3128b1334b522
kalbl <kallebladin@gmail.com>
2015-11-26 18:09:25 +0100

    Commented back hello world

--- include/HelloWorldCmd.h
+++ include/HelloWorldCmd.h
@@ -13,3 +13,37 @@ class HelloWorld : public MPxCommand {
   static void* creator();
 };
 #endif
+
+
+/*
+#ifndef BLENDNODE_H
+#define BLENDNODE_H
+ 
+#include <maya/MDataBlock.h>
+#include <maya/MDataHandle.h>
+#include <maya/MGlobal.h>
+#include <maya/MItGeometry.h>
+#include <maya/MMatrix.h>
+#include <maya/MPointArray.h>
+#include <maya/MStatus.h>
+ 
+#include <maya/MFnMesh.h>
+#include <maya/MFnNumericAttribute.h>
+#include <maya/MFnTypedAttribute.h>
+ 
+#include <maya/MPxDeformerNode.h>
+ 
+class BlendNode : public MPxDeformerNode {
+ public:
+  BlendNode() {};
+  virtual MStatus deform(MDataBlock& data, MItGeometry& itGeo,
+                         const MMatrix &localToWorldMatrix, unsigned int mIndex);
+  static void* creator();
+  static MStatus initialize();
+ 
+  static MTypeId id;
+  static MObject aBlendMesh;
+  static MObject aBlendWeight;
+};
+#endif
+*/
